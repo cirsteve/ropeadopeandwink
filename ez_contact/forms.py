f894c322a264d57719e222edda9416c010b7af7b
Steven Ciraolo <steven.ciraolo@gmail.com>
2011-09-11 02:30:42 -0700

    added ajax in contact forms

--- ez_contact/forms.py
+++ ez_contact/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=60)
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=True)
     subject = forms.CharField(max_length=60)
     message = forms.CharField(widget=forms.Textarea(attrs={'rows': 5,'cols':10}))
